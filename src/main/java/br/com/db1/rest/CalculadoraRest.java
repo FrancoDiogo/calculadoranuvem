9b566c26b406c429be030f62e8bca9df98abb109
FrancoDiogo <francodiogo797@gmail.com>
2018-09-12 00:34:28 -0300

    teste post

--- src/main/java/br/com/db1/rest/CalculadoraRest.java
+++ src/main/java/br/com/db1/rest/CalculadoraRest.java
@@ -1,6 +1,7 @@
 package br.com.db1.rest;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
@@ -13,14 +14,14 @@ public class CalculadoraRest {
 	
 	CalculadoraSimplesDao dao = new CalculadoraSimplesDao();
 	
-	@GET
+	@POST
 	@Path("dividir/{param}/{param2}")
 	public Response dividir(@PathParam("param") Double valor1, @PathParam("param2") Double valor2) {
 		Double result = dao.dividir(valor1, valor2);
 		return Response.status(200).entity("Resultado Calculadora Power: " + result).build();
 	}
 	
-	@GET
+	/*@GET
 	@Path("multiplicar/{param}/{param2}")
 	public Response multiplicar(@PathParam("param") Double valor1, @PathParam("param2") Double valor2) {
 		Double result = dao.multiplicar(valor1, valor2);
@@ -39,6 +40,6 @@ public class CalculadoraRest {
 	public Response subtrair(@PathParam("param") Double valor1, @PathParam("param2") Double valor2) {
 		Double result = dao.subtrair(valor1, valor2);
 		return Response.status(200).entity("Resultado Calculadora Power: " + result).build();
-	}
+	}*/
 
 }
